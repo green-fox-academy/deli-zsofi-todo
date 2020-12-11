4d8b80670c5640e517921b7ef8435c33d6212805
delizsofi <73013072+delizsofi@users.noreply.github.com>
2020-12-11 10:34:59 +0100

    start
    start

--- todo.mjs
+++ todo.mjs
@@ -1,21 +1,44 @@
 import minimist from 'minimist';
-
-//Fájlkezelés
-//létrehozás
-//olvasás
-//írás
+import fs from 'fs';
 
 
-const args = minimist( process.argv)
+
+if(typeof args.r === 'numebr') {
+    console.log( `Remove ${ arg.r}. todo`)
+}
+else if ( args.l === true) {
+    console.log( 'List Todos')
+}
+
+/*
+Miylen fájlok kellennek?
+- minden feature külön fájlbba
+- .txt fésjl, amiben tároljuk az adatot, a todokat
+- táblázatot txt
+    1 Kutyát sétáltatni    true
+    2 ...                   false
+- .cvs
+*/
+// ha -a hozzáad
+// todoList.add
+// a
+
+const args = minimist( process.argv);
 
 if(args.l) {
-    console.log("Kilistázza a feladatokat")
+    try {
+        console.log( fs.readFileSync('./todoList.txt').toString() );
+    }
+    catch( err ) {
+        console.log( 'FILE ERROR: ', 'Unable to read file: todoList.txt' );
+    
+    }
 } else if(args.a ) {
     console.log(`Új feladatot ad hozzá.`)
-} else if(args.r === 'almafa') {
-    console.log("locsol")
-}else if(args.c === 'almafa') {
-    console.log("locsol")
+} else if(args.r ) {
+    console.log(`Eltávolít egy feladatot.`)
+} else if(args.c ) {
+    console.log(`Teljesít egy feladatot.`)
 } else {
     console.log(`
     Parancssori Todo applikáció
